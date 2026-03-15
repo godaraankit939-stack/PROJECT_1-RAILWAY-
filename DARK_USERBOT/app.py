9cb5ea9959d2c61503bafdcfb3404aaaf523a2dc
godaraankit939-stack <godaraankit939@gmail.com>
2026-03-15 16:49:20 +0530

    Update app.py

--- DARK_USERBOT/app.py
+++ DARK_USERBOT/app.py
@@ -1,1 +1,16 @@
+from flask import Flask
+import threading
 
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "DARK USERBOT IS LIVE"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+def keep_alive():
+    t = threading.Thread(target=run)
+    t.start()
+  
